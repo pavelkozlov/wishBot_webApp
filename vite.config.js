b5bf1252a048f44808588e74611c989f05b0a452
Pavel Kozlov <it.pavelkozlov@gmail.com>
2025-05-25 17:35:42 +0300

    feat: initialize Telegram WebApp and configure allowed hosts in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/wishBot_webApp/',
-  server: {
-    allowedHosts: [
-      "pavelkozlov.github.io/wishBot_webApp"
-    ]
-  },
+  base: '/wishBot_webApp/', // 👈 имя репозитория на GitHub
   plugins: [
     vue(),
     vueDevTools(),
